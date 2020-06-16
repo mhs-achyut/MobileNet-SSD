45e6b18f745ba456ca59e6d7e134afcf1005d7bf
MHS-Achyut Boggaram <59655673+mhs-achyut@users.noreply.github.com>
2020-06-16 17:11:52 -0400

    fully functional train.sh

--- train.sh
+++ train.sh
@@ -1,23 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 if ! test -f example/MobileNetSSD_train.prototxt ;then
-	echo "error: example/MobileNetSSD_train.prototxt does not exist."
-	echo "please use the gen_model.sh to generate your own model."
+        echo "error: example/MobileNetSSD_train.prototxt does not exist."
+        echo "please use the gen_model.sh to generate your own model."
         exit 1
 fi
 
-if if [ ! -d "snapshot" ];then
-	echo "snapshot folder does not exist."
-	echo "using the default 73000_iter model"
-	mkdir -p snapshot
-	../../build/tools/caffe train -solver="solver_train.prototxt" \
-	-weights="mobilenet_iter_73000.caffemodel" \
-	-gpu 0 2>&1 | tee /home/$USER/caffe_mobilenet_training.log
+if [ ! -d "snapshot" ];then
+        echo "snapshot folder does not exist."
+        echo "using the default 73000_iter model"
+        mkdir -p snapshot
+        ../../build/tools/caffe train -solver="solver_train.prototxt" \
+        -weights="mobilenet_iter_73000.caffemodel" \
+        -gpu 0 2>&1 | tee /home/$USER/caffe_mobilenet_training.log
 else
-	unset -v latest
-	for file in "snapshot/*"; do
-	[[ $file -nt $latest ]] && latest=$file
-	done
-	../../build/tools/caffe train -solver="solver_train.prototxt" \
-	-weights="$latest" \
-	-gpu 0 2>&1 | tee /home/$USER/caffe_mobilenet_training.log
+        echo "snapshot folder exists"
+        max=-1
+        for file in snapshot/*.caffemodel
+        do
+                num=${file:24}
+                num=${num%.caffemodel}
+                echo "$num"
+                [ "$num" -gt "$max" ] && max=$num
+        done
+        echo "latest snapshot model is: $max"
+        ../../build/tools/caffe train -solver="solver_train.prototxt" \
+        -weights="snapshot/mobilenet_iter_$max.caffemodel" \
+        -gpu 0 2>&1 | tee /home/$USER/caffe_mobilenet_training.log
 fi
