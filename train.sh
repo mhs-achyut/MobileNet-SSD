5b1a7d053f9660b159c6137e09599863600e0e2d
MHS-Achyut Boggaram <59655673+mhs-achyut@users.noreply.github.com>
2020-06-16 16:26:14 -0400

    update train.sh to use an existing weights file

--- train.sh
+++ train.sh
@@ -4,7 +4,20 @@ if ! test -f example/MobileNetSSD_train.prototxt ;then
 	echo "please use the gen_model.sh to generate your own model."
         exit 1
 fi
-mkdir -p snapshot
-../../build/tools/caffe train -solver="solver_train.prototxt" \
--weights="mobilenet_iter_73000.caffemodel" \
--gpu 0 
+
+if ! test -f snapshot/*.caffemodel ;then
+	echo "snapshot folder does not exist."
+	echo "using the default 73000_iter model"
+	mkdir -p snapshot
+	../../build/tools/caffe train -solver="solver_train.prototxt" \
+	-weights="mobilenet_iter_73000.caffemodel" \
+	-gpu 0 2>&1 | tee /home/$USER/caffe_mobilenet_training.log
+else
+	unset -v latest
+	for file in "snapshot"/*; do
+	  [[ $file -nt $latest ]] && latest=$file
+	done
+	../../build/tools/caffe train -solver="solver_train.prototxt" \
+	-weights="$latest" \
+	-gpu 0 2>&1 | tee /home/$USER/caffe_mobilenet_training.log
+fi
